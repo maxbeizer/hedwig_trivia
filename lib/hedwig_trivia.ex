cf1b3861521aab04fc2810f1ddd925e8b3286f2e
Max Beizer <max.beizer@gmail.com>
2019-11-14 08:06:01 -0600

    Remove boilerplate

--- lib/hedwig_trivia.ex
+++ lib/hedwig_trivia.ex
@@ -1,18 +1,4 @@
 defmodule HedwigTrivia do
   @moduledoc """
-  Documentation for HedwigTrivia.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> HedwigTrivia.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/hedwig_trivia_test.exs
+++ test/hedwig_trivia_test.exs
@@ -1,8 +1,4 @@
 defmodule HedwigTriviaTest do
   use ExUnit.Case
   doctest HedwigTrivia
-
-  test "greets the world" do
-    assert HedwigTrivia.hello() == :world
-  end
 end
